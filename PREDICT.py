01d0230983652ffd0c3a7807a62464de20a2d43c
AmalenduR841 <130987816+AmalenduR841@users.noreply.github.com>
2023-05-02 11:11:19 +0530

    Update PREDICT.py

--- PREDICT.py
+++ PREDICT.py
@@ -30,9 +30,9 @@ naivebayes.fit(X_transformed, y_train)
 
 def app():
   st.header('Machine Prediction Demo')
-  input = st.text_area("Please enter the text", value="")
+  input=st.text_area("Please enter the text", value="")
   if st.button("Predict"):
     vec = vector.transform(input).toarray()
-    st.write('Headline:', input)
+    st.write('Headline:',input)
     st.write(str(list(naivebayes.predict(vec))[0]).replace('0', 'TECH').replace('1', 'BUSINESS').replace('2', 'SPORTS').replace('3','ENTERTAINMENT').replace('4','POLITICS'))
 app()
